59108adf7e42da03a142356e58536ce444d7d5bc
PSN <thepsnarayanan@gmail.com>
2023-02-24 09:41:08 +0530

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -rf /home/ec2-user/app/*
-rm -rf /home/ec2-user/app/.git*
-rm -rf /home/ec2-user/app/.coveragerc
-rm -rf /home/ec2-user/app/.flake8
-rm -rf /home/ec2-user/app/.gitignore
-rm -rf /home/ec2-user/app/.style.yapf
+#rm -rf /home/ec2-user/app/*
+#rm -rf /home/ec2-user/app/.git*
+#rm -rf /home/ec2-user/app/.coveragerc
+#rm -rf /home/ec2-user/app/.flake8
+#rm -rf /home/ec2-user/app/.gitignore
+#rm -rf /home/ec2-user/app/.style.yapf
